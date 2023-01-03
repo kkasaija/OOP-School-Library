871fb80a47f309640f7730ba05893b402bea20fc
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-03 23:36:07 +0300

    Add Capitalize and Trimmer Decorators

--- person.rb
+++ person.rb
@@ -44,3 +44,17 @@ class BaseDecorator < Nameable
     @nameable.correct_name
   end
 end
+
+class CapitalizeDecorator < BaseDecorator
+  def correct_name
+    @nameable.correct_name.capitalize
+  end
+end
+
+class TrimmerDecorator < BaseDecorator
+  def correct_name
+    return @nameable.correct_name[0...10] if @nameable.correct_name.length > 10
+
+    @nameable.correct_name
+  end
+end
