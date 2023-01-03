475f493162f8164939d805b204cedc295e9fcb8f
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-04 00:09:50 +0300

    Add classroom class

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+# classroom class
+class Classroom
+  attr_accessor :label, :student
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
