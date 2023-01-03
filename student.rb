b59880b6fee197c6dd7cdc483c336e0b4da8e2b2
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-03 06:36:42 +0300

    Add initializers to derived classes

--- student.rb
+++ student.rb
@@ -3,6 +3,11 @@ require './person'
 class Student < Person
   attr_accessor :classroom
 
+  def initialize(classroom)
+    super()
+    @classroom = classroom
+  end
+
   def play_hooky
     '¯\(ツ)/¯'
   end
--- teacher.rb
+++ teacher.rb
@@ -3,6 +3,11 @@ require './person'
 class Teacher < Person
   attr_accessor :specialization
 
+  def initialize(specialization)
+    super()
+    @specialization = specialization
+  end
+
   def can_use_services?
     true
   end
