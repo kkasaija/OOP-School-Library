da1be74f6c18ab1c8292a99132e9dd859301e952
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-12 16:42:46 +0100

    [persist_data]: create Store_rentals method and persist data

--- create_rental.rb
+++ create_rental.rb
@@ -1,4 +1,5 @@
 require './rental'
+require './persist.rb'
 
 class CreateRental
   def initialize(rent)
@@ -7,8 +8,15 @@ class CreateRental
 
   def create_rental(app)
     book, person, date = collect_data(app)
-    new_rental = Rental.new(date, app.books[book], app.people[person])
+    choosen_book = load_book_store[book]
+    new_book = Book.new(choosen_book['title'],choosen_book['author'] )
+
+    chosen_person = load_person_store[person]
+    new_person = Person.new(chosen_person['age'], chosen_person['name'], chosen_person['id'])
+
+    new_rental = Rental.new(date, new_book, new_person)
     update(new_rental)
+    store_rentals
   end
 
   private
@@ -29,5 +37,24 @@ class CreateRental
   def update(new_rental)
     @rentals << new_rental
     puts 'Rental created successfully'
+  end
+
+  def load_book_store
+    load_books = Persist.new('books.json')
+    load_books.load
+  end
+  def load_person_store
+    load_person = Persist.new('person.json')
+    load_person.load
+  end
+
+  def store_rentals
+    stored_rentals = Persist.new('rentals.json')
+    rentals = stored_rentals.load
+
+    @rentals.each do |rental|
+      rentals << {date: rental.date, book_title: rental.book.title, book_author: rental.book.author, person_name: rental.person.name, person_id: rental.person.id}
+    end
+    stored_rentals.save(rentals)
   end
 end
