8dab0ce25d74d9100b9012eb4d08e040dc33fe47
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-13 15:20:28 +0100

    [unit_test]: Update test for class_room_spec test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -3,9 +3,15 @@ require_relative '../classroom'
 
 describe Classroom do
   classroom = Classroom.new('Primary 7')
-  describe 'Classroom Details' do
+  context 'Classroom Details' do
     it 'should have a label of Primary 7' do
       expect(classroom.label).to eql('Primary 7')
+    end
+  end
+  context 'Add student:' do
+    it 'Should add Student' do
+      student = Student.new(30, 'Musinguzi', "Pri-5")
+      expect(student.classroom).to eq('Pri-5')
     end
   end
 end
