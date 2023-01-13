eb31fd420bdd46b2d955f777ab876130fa793d0a
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-13 15:20:37 +0100

    [unit_test]: Update test for person_spec test

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,12 +1,22 @@
 require_relative '../person'
+require_relative '../book.rb'
+require_relative '../rental.rb'
 
 describe Person do
   context 'Unit Test: ' do
+    new_person = Person.new(45, 'Prof. Blessing Blast', 234)
     it 'create a new Person when person class is instantiated' do
-      new_person = Person.new(45, 'Prof. Blessing Blast', 234)
       expect(new_person.age).to eq 45
       expect(new_person.name).to eq 'Prof. Blessing Blast'
       expect(new_person.id).to eq 234
+    end
+    it 'Person can use Service' do
+      expect(new_person.can_use_service).to eq true
+    end
+    it 'person can make rental' do
+      book = Book.new('Introduction to CS', 'Ruby Spec')
+      date = '23/01/12'
+      expect(new_person.add_rental(date, book).date).to eq('23/01/12')
     end
   end
 end
