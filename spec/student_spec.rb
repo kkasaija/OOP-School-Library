a11cdd04f73cf8ed8cd21b739d2c4cabc56c17b1
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-13 15:20:46 +0100

    [unit_test]: Update test for student_spec test

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../student'
 
 describe Student do
-  student = Student.new(30, 'Musinguzi', false)
+  student = Student.new(30, 'Musinguzi', "class-6")
   describe 'Student details' do
     it 'should be of type Student' do
       expect(student.age).to eql(30)
@@ -9,5 +9,9 @@ describe Student do
     it "should have a shape of '¯(ツ)/¯'" do
       expect(student.play_hooky).to eql('¯(ツ)/¯')
     end
+    it "should belong to 'class-6' classroom" do
+      expect(student.classroom).to eql("class-6")
+    end
+
   end
 end
