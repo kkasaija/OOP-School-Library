369a4ff059e63275caf2981bc4870f951cf74220
Kasaija Kenneth <kasaijak@gmail.com>
2023-01-13 18:45:38 +0300

    Modify of_age and correct_name method tests

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,6 +5,9 @@ require_relative '../rental'
 describe Person do
   context 'Unit Test: ' do
     new_person = Person.new(45, 'Prof. Blessing Blast', 234)
+    # new_person.send(:correct_name)
+    # new_person.send(:of_age)
+
     it 'create a new Person when person class is instantiated' do
       expect(new_person.age).to eq 45
       expect(new_person.name).to eq 'Prof. Blessing Blast'
@@ -18,11 +21,11 @@ describe Person do
       date = '23/01/12'
       expect(new_person.add_rental(date, book).date).to eq('23/01/12')
     end
-    it 'should return the correct age 45' do
-      expect(new_person.age).to eql(45)
+    it 'should return of_age  to be true' do
+      expect(new_person.send(:of_age)).to eql(true)
     end
     it 'should return the correct name' do
-      expect(new_person.name).to eql('Prof. Blessing Blast')
+      expect(new_person.send(:correct_name)).to eq('Prof. Blessing Blast')
     end
   end
 end
