83e08ebb8ddff6eabdcefcf3b1e8e883720f29e1
Inieke Sendy <ini.e.sendy@gmail.com>
2023-01-13 15:20:13 +0100

    [unit_test]: Update test for Book_spec testfile

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,4 +1,6 @@
 require_relative '../book'
+require_relative '../person.rb'
+require_relative '../rental.rb'
 
 describe Book do
   context 'Book Class Unit test' do
@@ -6,6 +8,17 @@ describe Book do
       book = Book.new('Introduction to CS', 'Ruby Spec')
       expect(book.title).to eq 'Introduction to CS'
       expect(book.author).to eq 'Ruby Spec'
+    end
+  end
+
+  context 'Add Rental Method' do
+    book = Book.new('Introduction to CS', 'Ruby Spec')
+    person = Person.new(45, 'Prof. Blessing Blast', 234)
+    date = '23/01/12'
+    it 'can Add Book rental' do
+      expect(book.add_rental(person, date).date).to eq('23/01/12')
+      expect(book.add_rental(person, date).book.title).to eq('Introduction to CS')
+      expect(book.add_rental(person, date).person.name).to eq('Prof. Blessing Blast')
     end
   end
 end
